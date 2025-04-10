91cee28c4ec965d82c591d0bb39c5edd8f2af129
MitsoaAllan <hei.mitsoa@gmail.com>
2025-04-10 10:25:18 +0300

    feat: Initial commit (application.properties, RestaurantManagement2025ApiSpringApplication.java , pom.xml)

---
+++ src/main/java/hei/school/restaurant/RestaurantManagement2025ApiSpringApplication.java
@@ -0,0 +1,13 @@
+package hei.school.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagement2025ApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantManagement2025ApiSpringApplication.class, args);
+    }
+
+}
