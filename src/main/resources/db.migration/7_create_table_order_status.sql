805839d337066d54b9dc5ac15373fb5135c8fa1f
MitsoaAllan <hei.mitsoa@gmail.com>
2025-04-11 01:20:57 +0300

    fix: refactor table order_status and dish_order_status

--- src/main/resources/db.migration/7_create_table_order_status.sql
+++ src/main/resources/db.migration/7_create_table_order_status.sql
@@ -1,10 +1,10 @@
 BEGIN;
-CREATE TYPE status as enum('CREATED','CONFIRMED','ON_GOING','FINISHED','DELIVERED');
+CREATE TYPE status as enum('CREATED','CONFIRMED','IN_PROGRESS','FINISHED','DELIVERED');
 COMMIT;
 
 CREATE TABLE IF NOT EXISTS order_status(
     id BIGSERIAL PRIMARY KEY,
     id_order int NOT NULL REFERENCES "order"(id),
-    status status NOT NULL DEFAULT 'CREATED'::status,
+    status status NOT NULL UNIQUE DEFAULT 'CREATED'::status,
     created_datetime timestamp DEFAULT now()
 );
--- src/main/resources/db.migration/9_create_table_dish_order_status.sql
+++ src/main/resources/db.migration/9_create_table_dish_order_status.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS dish_order_status(
     id_order int NOT NULL REFERENCES "order"(id),
     id_dish int NOT NULL REFERENCES dish(id),
-    status status NOT NULL DEFAULT 'CREATED'::status,
-    created_datetime timestamp DEFAULT now(),
-    PRIMARY KEY (id_order,id_dish)
+    status status NOT NULL UNIQUE DEFAULT 'CREATED'::status,
+    created_datetime timestamp DEFAULT now()
 );
