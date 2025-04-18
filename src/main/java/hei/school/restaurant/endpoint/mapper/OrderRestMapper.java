1f50ac2d6c2218251f919830e3d6303d59e16fa1
MitsoaAllan <hei.mitsoa@gmail.com>
2025-04-18 22:08:14 +0300

    chore: Update OrderRestMapper.java

--- src/main/java/hei/school/restaurant/endpoint/mapper/OrderRestMapper.java
+++ src/main/java/hei/school/restaurant/endpoint/mapper/OrderRestMapper.java
@@ -3,6 +3,7 @@ package hei.school.restaurant.endpoint.mapper;
 import hei.school.restaurant.dao.operations.order.DishOrderCRUDOperations;
 import hei.school.restaurant.dao.operations.order.OrderCRUDOperations;
 import hei.school.restaurant.endpoint.rest.DishOrderRest;
+import hei.school.restaurant.endpoint.rest.OrderBasicRest;
 import hei.school.restaurant.endpoint.rest.OrderRest;
 import hei.school.restaurant.model.order.Order;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +15,10 @@ import java.util.List;
 @Component
 public class OrderRestMapper {
     @Autowired private DishOrderRestMapper dishOrderRestMapper;
-    @Autowired private OrderCRUDOperations orderCRUDOperations;
-    @Autowired private DishOrderCRUDOperations dishOrderCRUDOperations;
 
     public OrderRest toRest(Order order) {
         List<DishOrderRest> dishOrders = order.getDishes().stream()
-                .map(dishOrderRestMapper::apply).toList();
+                        .map(dishOrderRestMapper::apply).toList();
 
         OrderRest orderRest = new OrderRest();
         orderRest.setId(order.getId());
@@ -28,5 +27,15 @@ public class OrderRestMapper {
         orderRest.setActualStatus(order.getActualStatus());
         orderRest.setDishOrders(dishOrders);
         return orderRest;
+    }
+
+    public OrderBasicRest toBasicRest(Order order) {
+        OrderBasicRest orderBasicRest = new OrderBasicRest();
+        orderBasicRest.setId(order.getId());
+        orderBasicRest.setReference(order.getReference());
+        orderBasicRest.setActualStatus(order.getActualStatus());
+        orderBasicRest.setTotalAmount(order.getTotalAmount());
+        orderBasicRest.setDishes(new ArrayList<>());
+        return orderBasicRest;
     }
 }
