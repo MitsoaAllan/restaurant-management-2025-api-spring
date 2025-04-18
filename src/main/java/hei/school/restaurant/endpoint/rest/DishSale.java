e706bce53c266f879d3902018ed94cf87d91a8b4
MitsoaAllan <hei.mitsoa@gmail.com>
2025-04-19 01:31:27 +0300

    feat: Adding DishSale.java Rest Model for Dish Sale

---
+++ src/main/java/hei/school/restaurant/endpoint/rest/DishSale.java
@@ -0,0 +1,13 @@
+package hei.school.restaurant.endpoint.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DishSale {
+    private int id;
+    private String name;
+}
